c561d2533eca066157628f1655c496626837fcb0
lqewo <vrkonosha@mail.rum>
2021-12-11 13:56:47 +0300

    added statistick and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,69 @@
 $(function() {
+  $('.crypt__inner').slick({
+    cssEase: 'linear',
+    centerMode: true,
+    centerPadding: '60px',
+    speed: 10000,
+    slidesToShow: 5,
+    arrows: false,
+    slidesToScroll: 1,
+    autoplaySpeed: 0,
+    autoplay: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 5
+        }
+      }
+    ]
+  });
 
+  $('.crypt__inner--reverse').slick({
+    cssEase: 'linear',
+    centerMode: true,
+    centerPadding: '60px',
+    speed: 10000,
+    slidesToShow: 5,
+    arrows: false,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 0,
+    autoplay: true,
+
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 5
+        }
+      }
+    ]
+  });
 });
